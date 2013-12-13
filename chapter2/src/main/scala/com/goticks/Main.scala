5a594f0cf1d4c0fed54cf29b94bda831b9b5ec37
yooneo <yooneo@icloud.com>
2013-12-13 11:29:11 -0800

    init

--- chapter2/src/main/scala/com/goticks/Main.scala
+++ chapter2/src/main/scala/com/goticks/Main.scala
@@ -7,8 +7,8 @@ import com.typesafe.config.ConfigFactory
 object Main extends App with SprayCanHttpServerApp {
   val config = ConfigFactory.load()
   val host = config.getString("http.host")
-  val port = config.getInt("http.port")
-
+  val port = config.getInt(("http.port"))
   val api = system.actorOf(Props(new RestInterface()), "httpInterface")
+
   newHttpServer(api) ! Bind(interface = host, port = port)
 }
